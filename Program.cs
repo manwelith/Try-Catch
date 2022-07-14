b588ac43a57d5464a77df4e198afae39ba9129d9
Soner A <107695479+manwelith@users.noreply.github.com>
2022-07-14 14:26:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
